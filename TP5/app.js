514b997984809561e253948688901618a8d5858f
alumno <52212702@ivt.edu.ar>
2026-06-04 09:07:37 -0300

    4 de junio

--- TP5/app.js
+++ TP5/app.js
@@ -1,8 +1,32 @@
-let numero1 =25;
-let numero2 =23;
-let resultado
+//ejercicio1 suma
+let numero1= 25;
+let numero2= 23;
+let suma
+let resultado = numero1+numero2
+console.log('el resultado de la suma es: '+resultado)
+alert('el resultado de la suma es: '+resultado)
 
-resultado = numero1 + numero2;
+//ejercicio1 resta
+let numero3= 5;
+let numero4= 15;
+let resta
+resultado = numero4-numero3
+console.log('el resultado de la resta es: '+resultado)
+alert('el resultado de la resta es: '+resultado)
 
-console.log(resultado);
-alert(resultado)
+//ejercicio1 multiplicación
+let numero5=20;
+let numero6=10;
+let multiplicacion
+resultado= numero5*numero6
+console.log('el resultado de la multiplicacion es: '+resultado)
+alert('el resultado de la multiplicacion es: '+resultado)
+
+//ejercicio1 division
+let numero7=20
+let numero8=2
+let division
+resultado= numero7/numero8
+console.log('el resultado de la division es: '+resultado)
+alert('el resultado de la division es: '+resultado)
+
