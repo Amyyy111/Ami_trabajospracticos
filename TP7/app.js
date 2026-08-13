8b2ece931151dd327fd653afd42737326f539d4e
alumno <52212702@ivt.edu.ar>
2026-08-13 09:09:55 -0300

    13/08/2026

---
+++ TP7/app.js
@@ -0,0 +1,9 @@
+let edadcliente = 14
+if (edadcliente >= 18) {
+    alert("podes ingresar")
+} else if (edadcliente > 14) {
+    alert('podes ingresar acompaña')
+}else{
+    alert('no podes ingresar')
+
+}
